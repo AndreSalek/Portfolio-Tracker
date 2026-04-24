6b908183d893caca77492f5f59024109354dd97d
Lukáš Man <man.lucas@seznam.cz>
2026-04-24 14:01:01 +0200

    Adjusted NavBar and added Model View

---
+++ PortfolioTracker.Web/ViewModels/LoginUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BackendLibrary;
+using Frontend.Common;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Frontend.ViewModels
+{
+    public class LoginUserViewModel
+    {
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        public bool IsLogged { get; set; }
+    }
+}
